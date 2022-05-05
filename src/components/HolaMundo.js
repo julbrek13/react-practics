9529853d4ec7758e39ffd9d1d22dd443ab7f554d
julbrek13 <julianmacarron@hotmail.com>
2022-05-05 16:27:08 -0300

    uso de fragmen y lugar donde pondremos las funciones

--- src/components/HolaMundo.js
+++ src/components/HolaMundo.js
@@ -4,19 +4,15 @@ import React from "react";
 //siempre e
 export default function HolaMundo(){
     
-    return( <div>
+    return( 
+    //si no quiero usar divs puedo hacer un fragmen que es lo siguiente
+    <>
         <h1>
             Hola Mundo
         </h1>
         <h2>
             Julián Macarron
         </h2>
-   </div>);
+   </>);
 }
 //recordemos que un fichero no puede tener dos export "defaults" solo uno
-export function AdiosMundo(){
-
-    return(<div>
-        <h3>Adios</h3>
-    </div>);
-}
