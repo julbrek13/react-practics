423edc4fe06a241290438f133e9b78c841be49ac
julbrek13 <julianmacarron@hotmail.com>
2022-05-05 15:44:20 -0300

    creo un componente y lo añado a app.js

---
+++ src/components/HolaMundo.js
@@ -0,0 +1,15 @@
+//los componentes tienen importaciones
+import React from "react";
+
+//siempre e
+export default function HolaMundo(){
+    
+    return( <div>
+        <h1>
+            Hola Mundo
+        </h1>
+        <h2>
+            Julián Macarron
+        </h2>
+   </div>);
+}
