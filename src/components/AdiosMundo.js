b525803273e1c1ce9b9392147ade3b5984f2ff14
julbrek13 <julianmacarron@hotmail.com>
2022-05-05 16:06:50 -0300

    ccreo un nuevo archivo js con el compónente de la manera correcta y cambio los parametros en app.js para que lo acepte

---
+++ src/components/AdiosMundo.js
@@ -0,0 +1,9 @@
+//esta seria la manera correcta, un componente por archivo js
+//ahora si podremos poner el default ya que es el default del archivo
+import React from "react";
+export default function AdiosMundo(){
+
+    return(<div>
+        <h3>Adios</h3>
+    </div>);
+}
